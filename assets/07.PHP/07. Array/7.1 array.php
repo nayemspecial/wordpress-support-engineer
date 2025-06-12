5008d9306a935aa0290136deeb45f8576f4f0f23
nayemspecial <nayemspecial@gmail.com>
2025-06-12 11:22:06 +0600

    PHP Class 7 Added

--- assets/07.PHP/07. Array/7.1 array.php
+++ assets/07.PHP/07. Array/7.1 array.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $fruits = array(
     "Mango", "Jackfruit", "Litchi"
 );
@@ -40,3 +41,61 @@ $data = ['name' => 'Abdur Rahim', 'age' => 25];
 $serialized = serialize($data);
 $json = json_encode($data);
 print_r($json);
+*/
+$fruits = ["Mango", "Jackfruit", "Litchi"];
+foreach ( $fruits as $fruit ) {
+    echo $fruit . "\n";
+}
+
+$person = [
+    'name' => 'Nayem',
+    'city' => 'Khulna',
+    'phone'=> '01900000000'
+];
+foreach ( $person as $key => $value ) {
+    echo "$key : $value\n" ;
+}
+print_r($person);
+
+$person = [
+    'name' => 'Nayem',
+    'city' => 'Khulna',
+    'phone'=> '01900000000'
+];
+
+unset($person['name']);
+print_r($person);
+
+$data = ['Mango', '', null];
+$filtered = array_filter($data);
+print_r($filtered);
+
+
+$user = ['name' => 'Nayem', 'email' => 'nayem@gmail.com'];
+extract($user);
+echo $email;
+
+$numbers = range(1, 15);
+$sliced = array_slice($numbers, 4, 1);
+print_r($numbers);
+print_r($sliced);
+
+$arr1 = ['Mango', 'Jackfruit'];
+$arr2 = ['Litchi', 'Pineapple'];
+$marged = array_merge($arr1, $arr2);
+print_r($marged);
+
+$fruits = ['Mango', 'Jackfruit'];
+if(in_array('Litchi', $fruits)){
+    echo "Found!";
+}else{
+    echo "Not Found!";
+}
+
+$arr1 = ['Mango', 'Litchi'];
+$arr2 = ['Litchi', 'Pineapple'];
+$differecnce = array_diff($arr1, $arr2);
+print_r($differecnce);
+
+$numbers = range(2, 15, 2);
+print_r($numbers);
