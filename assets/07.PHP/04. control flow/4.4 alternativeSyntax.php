e96ba17c2c836eb4cdba23d81f42180eddc3c79f
nayemspecial <nayemspecial@gmail.com>
2025-06-15 04:21:26 +0600

    class 8 added

---
+++ assets/07.PHP/04. control flow/4.4 alternativeSyntax.php
@@ -0,0 +1,50 @@
+<?php
+if($condition){
+    // Statements
+}else{
+    // Statements
+}
+
+// Alternative Syntax
+if($condition):
+    // Statements
+else:
+    // Statements
+endif;
+
+
+
+if($condition){
+    // Statements
+}elseif($anotherCondition){
+    // Statements
+}else{
+
+}
+
+// Alternative Syntax
+if($condition):
+    // Statements
+elseif($anotherCondition):
+    // Statements
+else:
+    // Statements
+endif;
+
+
+while ($condition){
+    // Statements
+}
+
+// Alternative Syntax
+while($condition):
+    // Statements
+endwhile;
+
+for( ; ; ){
+    // Statements
+}
+// Alternative syntax
+for( ; ; ):
+    // Statements
+endfor;
